8ed245693a2cf91a20843042c002aa434bdfbcb3
Ksdhake28 <kdhake289@gmail.com>
2024-10-24 12:51:22 +0530

    Shifting Backend to another repositry

--- Backend/docschema.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const docSchema = new mongoose.Schema({
-  id: Number,
-  aim: String,
-});
-
-module.exports = mongoose.model("assignments", docSchema);
--- DMSL/src/components/Rectangle.jsx
+++ DMSL/src/components/Rectangle.jsx
@@ -1,49 +1,57 @@
-import React, { Component } from 'react';
-import './Rectangle.css';
+import React, { Component } from "react";
+import "./Rectangle.css";
 
 class Rectangle extends Component {
-  render() {
-    return (
-      <div className="rectangle-container">
-        <div className="rectangle-content">
-          {this.props.Aim && (
-            <>
-              <h3>Aim:</h3>
-              <p>{this.props.Aim}</p>
-            </>
-          )}
-          {this.props.problemStatement && (
-            <>
-              <h3>Problem Statement:</h3>
-              <p>{this.props.problemStatement}</p>
-            </>
-          )}
-          {this.props.Objective && (
-            <>
-              <h3>Objective:</h3>
-              <p>{this.props.Objective}</p>
-            </>
-          )}
-          {this.props.Conclusion && (
-            <>
-              <h3>Conclusion:</h3>
-              <p>{this.props.Conclusion}</p>
-            </>
-          )}
-          {this.props.References && (
-            <>
-              <h3>REFERENCE BOOK:</h3>
-              <ul>
-                {this.props.References.map((reference, index) => (
-                  <li key={index}>{reference}</li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="rectangle-container">
+                <div className="rectangle-content">
+                    {this.props.Aim && (
+                        <>
+                            <h3>Aim:</h3>
+                            <p>{this.props.Aim}</p>
+                        </>
+                    )}
+                    {this.props.problemStatement && (
+                        <>
+                            <h3>Problem Statement:</h3>
+                            <p>{this.props.problemStatement}</p>
+                        </>
+                    )}
+                    {this.props.objective && (
+                        <>
+                            {console.log(this.props.objective)}
+                            <h3>Objective:</h3>
+                            <ol>
+                                {this.props.objective.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ol>
+                        </>
+                    )}
+
+                    {this.props.Conclusion && (
+                        <>
+                            <h3>Conclusion:</h3>
+                            <p>{this.props.Conclusion}</p>
+                        </>
+                    )}
+                    {this.props.References && (
+                        <>
+                            <h3>REFERENCE BOOK:</h3>
+                            <ul>
+                                {this.props.References.map(
+                                    (reference, index) => (
+                                        <li key={index}>{reference}</li>
+                                    )
+                                )}
+                            </ul>
+                        </>
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Rectangle;
