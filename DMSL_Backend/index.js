8ec59dded5d47dea06ab2ff3772b784a708cb02f
Ksdhake28 <kdhake289@gmail.com>
2024-10-25 10:44:12 +0530

    Hi message in index.js

--- DMSL_Backend/index.js
+++ DMSL_Backend/index.js
@@ -5,31 +5,31 @@ require("./connect.js");
 const doc = require("./docschema.js");
 
 const corsOptions = {
-  origin: "http://localhost:5173", 
-  methods: "GET,POST,PUT,DELETE", 
-  credentials: true, 
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
 };
 
 app.use(cors(corsOptions));
 
-// app.get("/", (req, resp) => {
-//   resp.send("hi");
-// });
+app.get("/", (req, resp) => {
+    resp.send("hi");
+});
 
 app.get("/practical/:id", async (req, resp) => {
-  const assign_id = req.params.id;
-  try {
-    const data = await doc.find({ id: assign_id });
-    console.log(data);
-    if (data) {
-      resp.status(200).json(data);
-    } else {
-      resp.status(404).send("Document not found");
+    const assign_id = req.params.id;
+    try {
+        const data = await doc.find({ id: assign_id });
+        console.log(data);
+        if (data) {
+            resp.status(200).json(data);
+        } else {
+            resp.status(404).send("Document not found");
+        }
+    } catch (error) {
+        resp.status(500).send("Server Error");
     }
-  } catch (error) {
-    resp.status(500).send("Server Error");
-  }
 });
 app.listen(3000, () => {
-  console.log("Listening on 3000");
-})
+    console.log("Listening on 3000");
+});
