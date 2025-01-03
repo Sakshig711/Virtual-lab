c5abc3301051363829668795a26321272fc1818b
Kshitij Dhake <141046195+Ksdhake28@users.noreply.github.com>
2025-01-03 18:41:05 +0530

    Update index.js

--- DMSL_Backend/index.js
+++ DMSL_Backend/index.js
@@ -5,7 +5,7 @@ require("./connect.js");
 const doc = require("./docschema.js");
 
 const corsOptions = {
-    origin: "https://virtual-lab-nine.vercel.app/",
+    origin: "https://virtual-lab-nine.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 };
