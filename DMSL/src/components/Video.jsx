53851d7722c27cc7a314cd1f1105ae2a1c2dab85
Sakshig711 <gangurdesakshi711@gmail.com>
2025-01-07 10:45:36 +0530

    Error handled

--- DMSL/src/components/Video.jsx
+++ DMSL/src/components/Video.jsx
@@ -1,27 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "./Video.css"
-// const VideoGallery = () => {
- 
-//   const [videos, setVideos] = useState([
-//     "https://dmsl-vlab.s3.ap-south-1.amazonaws.com/Assignment_5.mp4"
-//   ]);
-
-
-
-//   return (
-//     <div className="video-gallery">
-//       {videos.map((videoUrl, index) => (
-//         <div key={index} className="video-container">
-//           <video width="600" controls>
-//             <source src={videoUrl} type="video/mp4" />
-//           </video>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default VideoGallery;
+import "./Video.css";
 
 const VideoGallery = ({ id }) => {
   const defaultVideoUrl = "https://dmsl-vlab.s3.ap-south-1.amazonaws.com/Assignment_5.mp4";
@@ -29,11 +7,8 @@ const VideoGallery = ({ id }) => {
   const [videos, setVideos] = useState([
     `https://dmsl-vlab.s3.ap-south-1.amazonaws.com/Assignment_${id}.mp4`
   ]);
-<<<<<<< HEAD
-=======
 
   const handleVideoError = (index) => {
-
     setVideos((prevVideos) => {
       const updatedVideos = [...prevVideos];
       updatedVideos[index] = defaultVideoUrl;
@@ -41,7 +16,6 @@ const VideoGallery = ({ id }) => {
     });
   };
 
->>>>>>> 3709fab408143d2e2f1af7ddad66cec89df0be2c
   return (
     <div className="video-gallery">
       {videos.map((videoUrl, index) => (
