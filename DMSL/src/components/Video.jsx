ec83631a19bf1372ff66916a5528476a39aa937a
Sakshig711 <gangurdesakshi711@gmail.com>
2025-01-07 10:44:01 +0530

    Revert "Error handlede"
    This reverts commit a1565070886fdd5ac23e760c48c0efc1d51cbe9a.

--- DMSL/src/components/Video.jsx
+++ DMSL/src/components/Video.jsx
@@ -1,5 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import "./Video.css";
+import "./Video.css"
+// const VideoGallery = () => {
+ 
+//   const [videos, setVideos] = useState([
+//     "https://dmsl-vlab.s3.ap-south-1.amazonaws.com/Assignment_5.mp4"
+//   ]);
+
+
+
+//   return (
+//     <div className="video-gallery">
+//       {videos.map((videoUrl, index) => (
+//         <div key={index} className="video-container">
+//           <video width="600" controls>
+//             <source src={videoUrl} type="video/mp4" />
+//           </video>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default VideoGallery;
 
 const VideoGallery = ({ id }) => {
   const defaultVideoUrl = "https://dmsl-vlab.s3.ap-south-1.amazonaws.com/Assignment_5.mp4";
@@ -7,8 +29,11 @@ const VideoGallery = ({ id }) => {
   const [videos, setVideos] = useState([
     `https://dmsl-vlab.s3.ap-south-1.amazonaws.com/Assignment_${id}.mp4`
   ]);
+<<<<<<< HEAD
+=======
 
   const handleVideoError = (index) => {
+
     setVideos((prevVideos) => {
       const updatedVideos = [...prevVideos];
       updatedVideos[index] = defaultVideoUrl;
@@ -16,6 +41,7 @@ const VideoGallery = ({ id }) => {
     });
   };
 
+>>>>>>> 3709fab408143d2e2f1af7ddad66cec89df0be2c
   return (
     <div className="video-gallery">
       {videos.map((videoUrl, index) => (
