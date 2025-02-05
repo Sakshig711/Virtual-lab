6150842f5a4d072f90a3ab4df393e66d44789311
Atharva <atharva.studiumtech@gmail.com>
2025-02-05 15:35:51 +0530

    website made responsive

--- DMSL/src/components/Nav.jsx
+++ DMSL/src/components/Nav.jsx
@@ -1,32 +1,54 @@
-import React from "react";
-import pictlogo from '../assets/pictLogo.png'
-import polygon from '../assets/poly4.svg';
+import React, { useState } from "react";
+import pictlogo from "../assets/pictLogo.png";
+import polygon from "../assets/poly4.svg";
 import "./css/Nav.css";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div className="frame">
-          <div className="div">
-            <div className="nav-bar">
-              <div className="overlap-group">
-               {/* <img className="polygon" alt="Polygon" src={polygon3} />
-                <img className="img" alt="Polygon" src={polygon2} />   */}
-                <img className="poly" alt="Polygon" src ={polygon} />
-                <p className="text-wrapper">Pune Institute Of Computer Technology</p>
-                <p className="p">A Virtual Lab for Database Management System</p>
-                <img className="pictlogo" alt="Pictlogo" src={pictlogo} />
+  const [menuOpen, setMenuOpen] = useState(false);
 
-                <div className="nav-links">
-                  <Link to="/">Home</Link>
-                  <Link to="/aboutus">About Us</Link>
-                  <Link to="/assignmentlist">List</Link>
-                </div>
-              </div>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="frame">
+      <div className="div">
+        <div className="nav-bar">
+          <div className="overlap-group">
+            <img className="poly" alt="Polygon" src={polygon} />
+            <p className="text-wrapper">
+              Pune Institute Of Computer Technology
+            </p>
+            <p className="p">A Virtual Lab for Database Management System</p>
+            <img className="pictlogo" alt="Pictlogo" src={pictlogo} />
+
+            {/* Desktop Navigation Links */}
+            <div className="nav-links">
+              <Link to="/">Home</Link>
+              <Link to="/aboutus">About Us</Link>
+              <Link to="/assignmentlist">Assignment List</Link>
+            </div>
+
+            {/* Mobile Hamburger Icon */}
+            <button className="menu-icon" onClick={toggleMenu}>
+              ☰
+            </button>
+
+            {/* Mobile Navigation Links */}
+            <div
+              className={`nav-links-mobile ${menuOpen ? "active" : ""}`}
+              onClick={() => setMenuOpen(false)}
+            >
+              <Link to="/">Home</Link>
+              <Link to="/aboutus">About Us</Link>
+              <Link to="/assignmentlist">AssignmentList</Link>
             </div>
           </div>
         </div>
-      );
-}
+      </div>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
